bbf6d11395e3094cdf89cd54ed0e0fb717012024
LaurenceJorissen <99961435+LaurenceJorissen@users.noreply.github.com>
2023-12-16 17:26:46 +0100

    Add files via upload

--- Gsr_acc_data.py
+++ Gsr_acc_data.py
@@ -1,137 +1,205 @@
-# vragen voor thijs: (Ge kunt eerst zelf over een paar vragen nadenken voordat ge ze stelt tho kan zijn dat mn vragen retarded zijn)
-    # moet ge met platformio alleen maar raw sensor data naar de influx sturen? of mag men al bewerkingen doen in platformio? (mss is het doorsturen van data dan trager?)
-    # Is het de bedoeling om bij de final product ervoor te zorgen dat we onze laptop totaal niet meer moeten gebruiken? dus zonder het opstarten van containers? (lijkt me onlogisch)
-    # Vraag of ze van die electrode skin tape dingen kunnen geven / als ze dat hebben.
-    # kunnen we de verwerkte data ook via een website (html) displayen ipv een app?
-    # of we zo een batterij om aan het bordje te hangen zouden kunnen krijgen
-    # hoe belangrijk is het gebruik van SPI's? we zouden de gsr sensor op een miso kunnen zetten maar heeft dat veel nut als het maar 1 sensor is die op deze lijn staat? (mss staat antwoord in de slides moet ge eens nakijken)
-    # kunnen we verschillende sample rates gebruiken op 1 ESP bordje? Omdat een goeie sample rate voor gsr tussen 1-10 Hz is en die van ppg tussen 20 - 50 Hz
-    
-    # ...
-
-# WAT ER IN DE CODE GEBEURT
-# Code is heel gelijkaardig aan de code van de opdracht met ECG data
-# In deze code halen we nu gewoon alle waardes uit de influx, dus: Accelerometer (x,y,z - beweging), ppg meting, gsr meting)
-# elke soort data zetten we in een aparte dataframe
-# waardes die we willen gebruiken zetten we om naar numeric (kan zijn dat da voor de value niet moet, zou ge eens kunnen proberen ofdat da zo is)
-# daarna berekenen we maar snel het gemiddelde om te kijken of we effectief iets kunnen doen met die data
-# ...
-
-# WAT TE DOEN
-# probeer gsr en ppg data te verwerken
-# kijk welke waarden men uit gsr en ppg data kan halen waarmee we de link met stress kunnen leggen
-# als ge gewoon al weet welke waardes we moeten meten (gelijk oxygen lvl, hartslag, bloodflow) kunt  ge ze daarna filteren en printen
-# dan zitten we al ver
-# ge zou de snelheid uit een accelerometer kunnen bepalen maar dat is al moeilijker denk ik (als het zelfs gaat om snelheid te berekenen)
-# het zou kunnen zijn dat ge in uw main.cpp uw setup van uw ppg signaal moet aanpassen naarmate welke metingen ge wilt gaan doen (ale dat gok ik toch)
-
-# ALS WIFI/INFLUX NIET WERKT
-# in uw main.cpp file line 65 in comment zetten en dan de code eens proberen te runnen
-# laptop met zelfde wifi als bordje verbinden om met influx te connecten
-# docker opstarten om met influx te connecten
-
 import influxdb_client
 import os
 import time
 import pandas as pd
+import heartpy as hp
 import matplotlib.pyplot as plt
 from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 import random
 import Preferences
 import numpy as np
+from scipy.signal import butter, lfilter
+
 
 client = influxdb_client.InfluxDBClient(url=Preferences.url, token=Preferences.token, org=Preferences.org)
 
-sample_rate = 10
+sample_rate = 100
 bucket = "Raw_Data_ecg_bloeddruk"
 
-while(1):
 
-    query_api = client.query_api()
+query_api = client.query_api()
 
-    combined_query = """from(bucket: "Raw_Data_ecg_bloeddruk")
-    |> range(start: -1m)
-    |> filter(fn: (r) =>
-        (r._measurement == "GSR_Data") or
-        (r._measurement == "ACC_Data") or
-        (r._measurement == "PPG_Data")
-    )
-    """
+combined_query = """from(bucket: "Raw_Data_ecg_bloeddruk")
+|> range(start: -30m)
+|> filter(fn: (r) =>
+    (r._measurement == "GSR_Data") or
+    (r._measurement == "ACC_Data") or
+    (r._measurement == "PPG_Data")
+)
+"""
 
-    tables = query_api.query(combined_query, org="EHealth")
+tables = query_api.query(combined_query, org="EHealth")
 
+def extract_data_from_records(tables):
     gsr_data = []
     x_acc_data = []
     y_acc_data = []
     z_acc_data = []
-    ppg_data = []
+    bpm = []
+    sp02 = []
 
     for table in tables:
         for record in table.records:
             # Extracting time, field, and value from the record
-            if record.get_measurement() == "GSR_Data":
-                gsr_data.append((record.get_time(), record.get_field(), record.get_value()))
-            elif record.get_measurement() == "ACC_Data" and record.get_field() == "acceleration_X":
-                x_acc_data.append((record.get_time(), record.get_field(), record.get_value()))
-            elif record.get_measurement() == "ACC_Data" and record.get_field() == "acceleration_Y":
-                y_acc_data.append((record.get_time(), record.get_field(), record.get_value()))
-            elif record.get_measurement() == "ACC_Data" and record.get_field() == "acceleration_Z":
-                z_acc_data.append((record.get_time(), record.get_field(), record.get_value()))
-            elif record.get_measurement() == "PPG_Data":
-                ppg_data.append((record.get_time(), record.get_field(), record.get_value()))
+            measurement = record.get_measurement()
+            field = record.get_field()
+            value = record.get_value()
+            time = record.get_time()
 
+            if measurement == "GSR_Data":
+                gsr_data.append((time, measurement, value))
+            elif measurement == "ACC_Data" and field == "acceleration_X":
+                x_acc_data.append((time, measurement, value))
+            elif measurement == "ACC_Data" and field == "acceleration_Y":
+                y_acc_data.append((time, measurement, value))
+            elif measurement == "ACC_Data" and field == "acceleration_Z":
+                z_acc_data.append((time, measurement, value))
+            elif measurement == "PPG_Data" and field == "bpm":
+                bpm.append((time, measurement, value))
+            elif measurement == "PPG_Data" and field == "sp02":
+                sp02.append((time, measurement, value))
+
+    return gsr_data, x_acc_data, y_acc_data, z_acc_data, bpm, sp02
+
+# Function to convert data to DataFrames and make values numeric
+def convert_to_numeric_and_create_df(tables):
+    gsr_data, x_acc_data, y_acc_data, z_acc_data, bpm, sp02 = extract_data_from_records(tables)
+    
+    # Convert data to DataFrames
     df_gsr = pd.DataFrame(gsr_data, columns=["Time", "Measurement", "Value"])
     df_acc_x = pd.DataFrame(x_acc_data, columns=["Time", "Measurement", "Value"])
     df_acc_y = pd.DataFrame(y_acc_data, columns=["Time", "Measurement", "Value"])
     df_acc_z = pd.DataFrame(z_acc_data, columns=["Time", "Measurement", "Value"])
-    df_ppg = pd.DataFrame(ppg_data, columns=["Time", "Measurement", "Value"])
+    bpm = pd.DataFrame(bpm, columns=["Time", "Measurement", "Value"])  # DataFrame for rValue
+    sp02 = pd.DataFrame(sp02, columns=["Time", "Measurement", "Value"])  # DataFrame for irValue
 
-    df_gsr['Value'] = pd.to_numeric(df_gsr['Value'], errors='coerce')
-    df_gsr['Time'] = pd.to_numeric(df_gsr['Time'], errors='coerce')
-    data_acc_x = pd.to_numeric(df_acc_x['Value'], errors='coerce')
-    data_acc_y = pd.to_numeric(df_acc_y['Value'], errors='coerce')
-    data_acc_z = pd.to_numeric(df_acc_z['Value'], errors='coerce')
-    df_ppg['Value'] = pd.to_numeric(df_ppg['Value'], errors='coerce')
-    df_ppg['Time'] = pd.to_numeric(df_ppg['Time'], errors='coerce')
+    # Print the created DataFrames
+    print("DataFrame for GSR data:")
+    print(df_gsr)
 
+    print("\nDataFrame for X-axis Movement data:")
+    print(df_acc_x)
 
-    mean_value_ppg = df_ppg['Value'].mean()
-    mean_value_gsr = df_gsr['Value'].mean()
-    median_value = df_gsr['Value'].median()
-    min_value = df_gsr['Value'].min()
-    max_value = df_gsr['Value'].max()
-    std_deviation = df_gsr['Value'].std()
+    print("\nDataFrame for Y-axis Movement data:")
+    print(df_acc_y)
 
-    print("Mean gsr:", mean_value_gsr)
-    print("Mean ppg:", mean_value_ppg)
+    print("\nDataFrame for Z-axis Movement data:")
+    print(df_acc_z)
+
+    print("\nDataFrame for BPM data:")
+    print(bpm)
+
+    print("\nDataFrame for SpO2 data:")
+    print(sp02)
     
-    '''
-    print("Median:", median_value)
-    print("Min:", min_value)
-    print("Max:", max_value)
-    print("Standard Deviation:", std_deviation)
+    # Convert "Value" column to numeric in each DataFrame
+    df_gsr["Value"] = pd.to_numeric(df_gsr["Value"], errors="coerce")
+    df_acc_x["Value"] = pd.to_numeric(df_acc_x["Value"], errors="coerce")
+    df_acc_y["Value"] = pd.to_numeric(df_acc_y["Value"], errors="coerce")
+    df_acc_z["Value"] = pd.to_numeric(df_acc_z["Value"], errors="coerce")
+    bpm["Value"] = pd.to_numeric(bpm["Value"], errors="coerce")
+    sp02["Value"] = pd.to_numeric(sp02["Value"], errors="coerce")
 
-    plt.figure(figsize=(12, 4))
-    plt.plot(df_gsr['Time'], df_gsr['Value'])
-    plt.xlabel('Time')
-    plt.ylabel('GSR Value')
-    plt.title('GSR Data Over Time')
+    return df_gsr, df_acc_x, df_acc_y, df_acc_z, bpm, sp02
 
-    rolling_mean = df_gsr['Value'].rolling(window=10).mean()  # Adjust window size as needed
-    plt.plot(df_gsr['Time'], rolling_mean, label='Rolling Mean')
-    plt.legend()
-    plt.show()
-    '''
-    '''
-    client = influxdb_client.InfluxDBClient(url=Preferences.url, token=Preferences.token, org=Preferences.org)
-    bucket = "Clear_Data_ecg_bloeddruk"
-    write_api = client.write_api(write_options=SYNCHRONOUS)
-    random_patient_number = random.randint(1,100)
-    point = (
+# Example usage
+# Assuming 'tables' variable contains the data
 
-    )
-    # Write the data point to InfluxDB
-    write_api.write(bucket=bucket, org="EHealth", record=point)
-    '''
-    time.sleep(60)
+# df_gsr, df_acc_x, df_acc_y, df_acc_z, df_ppg = convert_to_numeric_and_create_df(tables)
+gsr_data, x_acc_data, y_acc_data, z_acc_data, bpm, sp02 = convert_to_numeric_and_create_df(tables)
+filtered_bpm = bpm.copy()
+while True:
+    # Store the length of the previous 'bpm' DataFrame
+    prev_length = len(filtered_bpm)
+
+    # Calculate differences between consecutive filtered_bpm values
+    filtered_bpm_diff = np.diff(filtered_bpm['Value'])
+
+    # Find indices where the drop is greater than 20 filtered_bpm or rise is greater than 20 filtered_bpm
+    indices_of_drop = np.where(filtered_bpm_diff < -25)[0]
+    indices_of_rise = np.where(filtered_bpm_diff > 25)[0]
+
+    # Combine the indices of drops and rises
+    indices_to_remove = np.concatenate((indices_of_drop, indices_of_rise)) + 1
+
+    # Ensure the indices to remove are within the DataFrame index range
+    indices_to_remove = indices_to_remove[indices_to_remove < len(filtered_bpm)]
+
+    # Check if any indices need to be removed
+    if len(indices_to_remove) > 0:
+        # Check if the indices to remove actually exist in the DataFrame index
+        indices_to_remove = indices_to_remove[indices_to_remove < len(filtered_bpm.index)]
+
+        # Remove instances of drops or rises from the 'filtered_bpm' DataFrame
+        filtered_bpm = filtered_bpm.drop(filtered_bpm.index[indices_to_remove])
+    else:
+        break  # If no drops or rises are found, exit the loop
+
+    # Check if no elements were removed in this iteration
+    if len(filtered_bpm) == prev_length:
+        break  # If no elements were removed, exit the loop
+
+    print("\nsp02:")
+    print(sp02['Value'].mean())
+
+    print("\nbpm:")
+    print(filtered_bpm['Value'].mean())
+
+filtered_bpm = filtered_bpm[(filtered_bpm['Value'] >= 40) & (filtered_bpm['Value'] <= 180)]
+
+print("\nsp02:")
+print(sp02['Value'].mean())
+
+print("\nbpm:")
+print(filtered_bpm['Value'].mean())
+
+rolling_mean = gsr_data['Value'].rolling(window=10).mean()  # Adjust window size as needed
+# Create a figure with multiple subplots
+fig, axs = plt.subplots(nrows=4, ncols=1, figsize=(16, 24))
+
+# Plot GSR data
+axs[0].plot(gsr_data['Value'], color='orange')
+axs[0].legend()
+axs[0].grid()
+axs[0].set_title('GSR Data')
+
+# Plot BPM data
+axs[1].plot(filtered_bpm['Value'].rolling(window=50).mean(), color='red', label = 'rolling mean')
+axs[1].plot(bpm['Value'], color='black', label = 'raw')
+axs[1].plot(filtered_bpm['Value'], color='green', label = 'filtered')
+axs[1].legend()
+axs[1].grid()
+axs[1].set_title('BPM Data')
+
+# Plot SpO2 data
+axs[2].plot(sp02['Value'].rolling(window=50).mean(),color='blue', label = 'rolling mean')
+axs[2].plot(sp02['Value'], color='black',label = 'raw')
+axs[2].legend()
+axs[2].grid()
+axs[2].set_title('SpO2 Data')
+
+# Plot x-axis Movement data
+axs[3].plot(x_acc_data['Value'], label='x-movement')
+# Plot z-axis Movement data
+axs[3].plot(z_acc_data['Value'], label='z-movement')
+# Plot y-axis Movement data
+axs[3].plot(y_acc_data['Value'], label='y-movement')
+axs[3].legend()
+axs[3].grid()
+
+# Adjust layout and display
+plt.tight_layout(pad=5.0)
+plt.show()
+
+'''
+client = influxdb_client.InfluxDBClient(url=Preferences.url, token=Preferences.token, org=Preferences.org)
+bucket = "Clear_Data_ecg_bloeddruk"
+write_api = client.write_api(write_options=SYNCHRONOUS)
+random_patient_number = random.randint(1,100)
+point = (
+
+)
+# Write the data point to InfluxDB
+write_api.write(bucket=bucket, org="EHealth", record=point)
+'''
--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include <SPI.h>
 #include <esp32-hal-gpio.h>
 #include <HardwareSerial.h>
 
@@ -19,10 +18,11 @@
 #include <Adafruit_ADXL345_U.h>
 #include <Adafruit_NeoPixel.h>
 #include "MAX30105.h"
-
+#include "spo2_algorithm.h"
+#include "heartRate.h"
 
 unsigned long PrevMicros = 0;
-uint16_t Sample_TimeUS = 1000*1000/10; //Sample time in microseconds
+uint16_t Sample_TimeUS = 1000*1000/100; //Sample time in microseconds
 MAX30105 ppgSensor;
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 
@@ -32,16 +32,16 @@ Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
 #define I2C_SDA_PIN 8
 #define I2C_SCL_PIN 10
 // WiFi AP SSID
-#define WIFI_SSID SSID_Uhasselt
+#define WIFI_SSID SSID_thuis
 // WiFi password
-#define WIFI_PASSWORD Password_Uhasselt
-#define INFLUXDB_URL url_Uhasselt
+#define WIFI_PASSWORD Password_thuis
+#define INFLUXDB_URL url_thuis
 #define INFLUXDB_TOKEN "H1DRZGTLN5Dcau4KwiHIdGnZFOe-ZSJf4m9uNJ6Uu99LYf3_wE_JPrWL29SXwybYPDL6HtnuTJO2fatfCLBhhg==" 
 #define INFLUXDB_ORG "EHealth"
 #define INFLUXDB_BUCKET "Raw_Data_ecg_bloeddruk"
 
 // Time zone info
-#define TZ_INFO "UTC-1"
+#define TZ_INFO "UTC1"
 
 // Declare InfluxDB client instance with preconfigured InfluxCloud certificate
 InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
@@ -58,10 +58,31 @@ Point ppgmeterBuffer("PPG_buffer");
 const int GSR=1; //
 int sensorValue=0; //
 
+#define MAX_BRIGHTNESS 255
+
+uint32_t irBuffer[100]; //infrared LED sensor data
+uint32_t redBuffer[100];  //red LED sensor data
+
+int32_t bufferLength; //data length
+int32_t spo2; //SPO2 value
+int8_t validSPO2; //indicator to show if the SPO2 calculation is valid
+int32_t heartRate; //heart rate value calcualated as per Maxim's algorithm
+int8_t validHeartRate; //indicator to show if the heart rate calculation is valid
+
+
+long lastBeat = 0; //Time at which the last beat occurred
+
+float beatsPerMinute; //stores the BPM as per custom algorithm
+int beatAvg = 0, sp02Avg = 0; //stores the average BPM and SPO2 
+float ledBlinkFreq; //stores the frequency to blink the pulseLED
+
+#define MAX30105_I2C_ADDRESS 0x57 // MAX30105 sensor I2C address
+#define ADXL345_I2C_ADDRESS 0x53  // or 0x1D depending on the ADXL345 address configuration
+
 void setup() {
   delay(10000);
   Serial.begin(115200);
-  
+
   WiFi.setTxPower(WIFI_POWER_8_5dBm);
   WiFi.mode(WIFI_STA);
   wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
@@ -89,7 +110,7 @@ void setup() {
   client.setWriteOptions(
       WriteOptions()
           .writePrecision(WritePrecision::MS)
-          .batchSize(1000)
+          .batchSize(240)
           .bufferSize(2500)
           .flushInterval(100)
   );
@@ -98,36 +119,37 @@ void setup() {
   client.setHTTPOptions(
       HTTPOptions().connectionReuse(true)
   );  
-
-
+  
   Serial.println("Accelerometer Test");
   Serial.println("");
  
   Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN); // Initialize I2C on GPIO pins for SDA and SCL
-  accel.begin();
-
+  
   if (!accel.begin()) {
     Serial.println("Ooops, no ADXL345 detected... Check your wiring!");
-    while (1);
+    setup();
   }
 
   accel.setRange(ADXL345_RANGE_16_G);
 
-  ppgSensor.begin();
-
-  if (!ppgSensor.begin()) {
+  Serial.print("Initializing Pulse Oximeter..");
+  if (!ppgSensor.begin(Wire, I2C_SPEED_FAST)) {
     Serial.println("Error initializing PPG sensor");
-    while (1);
+    while(1);
   }
 
-  ppgSensor.setup(); //Configure sensor with default settings
-  ppgSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
-  ppgSensor.setPulseAmplitudeGreen(0); //Turn off Green LED
+  byte ledBrightness = 50; //Options: 0=Off to 255=50mA
+  byte sampleAverage = 1; //Options: 1, 2, 4, 8, 16, 32
+  byte ledMode = 2; //Options: 1 = Red only, 2 = Red + IR, 3 = Red + IR + Green
+  byte sampleRate = 100; //Options: 50, 100, 200, 400, 800, 1000, 1600, 3200
+  int pulseWidth = 69; //Options: 69, 118, 215, 411
+  int adcRange = 4096; //Options: 2048, 4096, 8192, 16384
 
-  Serial.println("");
+  ppgSensor.setup(ledBrightness, sampleAverage, ledMode, sampleRate, pulseWidth, adcRange); //Configure sensor with these settings
+
 }
 
-void sendDataToInfluxDB(float gsrValue, float acceleration_X, float acceleration_Y, float acceleration_Z, float ppgData) {
+void sendDataToInfluxDB(float gsrValue, float acceleration_X, float acceleration_Y, float acceleration_Z, float sp02Avg, float beatAvg) {
     // Add fields to the point
     if (client.isBufferEmpty()) {
       Serial.print("Buffer is empty\n");
@@ -137,7 +159,8 @@ void sendDataToInfluxDB(float gsrValue, float acceleration_X, float acceleration
       acceleratorBuffer.addField("acceleration_X", acceleration_X);
       acceleratorBuffer.addField("acceleration_Y", acceleration_Y);
       acceleratorBuffer.addField("acceleration_Z", acceleration_Z);
-      ppgmeterBuffer.addField("ppg_value", ppgData);
+      ppgmeterBuffer.addField("sp02", sp02Avg);
+      ppgmeterBuffer.addField("bpm", beatAvg);
       client.writePoint(ppgmeterBuffer);
       client.writePoint(sensorNetworks); // Write point into buffer
       client.writePoint(acceleratorBuffer); // Write point into buffer
@@ -147,7 +170,8 @@ void sendDataToInfluxDB(float gsrValue, float acceleration_X, float acceleration
     accelerator.addField("acceleration_X", acceleration_X);
     accelerator.addField("acceleration_Y", acceleration_Y);
     accelerator.addField("acceleration_Z", acceleration_Z);
-    ppgmeter.addField("ppg_value", ppgData);
+    ppgmeter.addField("sp02", sp02Avg);
+    ppgmeter.addField("bpm", beatAvg);
     // Write the point to InfluxDB
     if (client.writePoint(sensor) and client.writePoint(accelerator) and client.writePoint(ppgmeter)) {
       //Serial.println("Data sent to InfluxDB successfully!");
@@ -164,29 +188,82 @@ void sendDataToInfluxDB(float gsrValue, float acceleration_X, float acceleration
 }
 
 void loop(){
-  if (micros() >= PrevMicros + Sample_TimeUS){
-    PrevMicros = micros();
-    /* Get a new sensor event */ 
+  bufferLength = 100; //buffer length of 100 stores 4 seconds of samples running at 25sps
+  
+  //read the first 100 samples, and determine the signal range
+  for (byte i = 0 ; i < bufferLength ; i++)
+  {
+    while (ppgSensor.available() == false) //do we have new data?
+      ppgSensor.check(); //Check the sensor for new data
+  
+    redBuffer[i] = ppgSensor.getIR();
+    irBuffer[i] = ppgSensor.getRed();
+    ppgSensor.nextSample(); //We're finished with this sample so move to next sample
+  
+    Serial.print(F("red: "));
+    Serial.print(redBuffer[i], DEC);
+    Serial.print(F("\t ir: "));
+    Serial.println(irBuffer[i], DEC);
+  }
+  
+  //calculate heart rate and SpO2 after first 100 samples (first 4 seconds of samples)
+  maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+  
+  //Continuously taking samples from MAX30102.  Heart rate and SpO2 are calculated every 1 second
+  while (1)
+  {
+    //dumping the first 25 sets of samples in the memory and shift the last 75 sets of samples to the top
+    for (byte i = 25; i < 100; i++)
+    {
+      redBuffer[i - 25] = redBuffer[i];
+      irBuffer[i - 25] = irBuffer[i];
+    }
+    long irValue;
+    //take 25 sets of samples before calculating the heart rate.
+    for (byte i = 75; i < 100; i++)
+    {
+      while (ppgSensor.available() == false) //do we have new data?
+        ppgSensor.check(); //Check the sensor for new data
+
+    
+      redBuffer[i] = ppgSensor.getRed();
+      irBuffer[i] = ppgSensor.getIR();
+      ppgSensor.nextSample(); //We're finished with this sample so move to next sample
+
+      irValue = irBuffer[i];
+
+      //Calculate BPM independent of Maxim Algorithm. 
+      if (checkForBeat(irValue) == true && irValue > 50000)
+      {
+        //We sensed a beat!
+        long delta = millis() - lastBeat;
+        lastBeat = millis();
+      
+        beatsPerMinute = 60 / (delta / 1000.0);
+        beatAvg = (beatAvg+beatsPerMinute)/2;
+      }
+      if(millis() - lastBeat > 10000)
+      {
+        beatsPerMinute = 0;
+        beatAvg = (beatAvg+beatsPerMinute)/2;
+      }
+    }
+  
+    //After gathering 25 new samples recalculate HR and SP02
+    maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
+  
+    //Calculates average SPO2 to display smooth transitions on Blynk App
+    if(validSPO2 == 1 && spo2 < 100 && spo2 > 0 && irValue > 50000)
+    {
+      sp02Avg = (sp02Avg+spo2)/2;
+    }
+
     sensors_event_t event; 
     accel.getEvent(&event);
     float sensorValue = analogRead(GSR);
-    float ppgData = ppgSensor.getIR();
     float acceleration_X = event.acceleration.x;
     float acceleration_Y = event.acceleration.y;
     float acceleration_Z = event.acceleration.z;
-    
-    /*
-    Serial.print("\ngsrValue: " + String(sensorValue));
-    Serial.print("");
-    Serial.print("\n[");
-    Serial.print(event.acceleration.x); Serial.print(", ");
-    Serial.print(event.acceleration.y); Serial.print(", ");
-    Serial.print(event.acceleration.z); Serial.println("] ");
-    Serial.print("ppgValue: " + String(ppgData));
-    Serial.print("");
-    delay(500);
-    */
-    
-    sendDataToInfluxDB(sensorValue, acceleration_X, acceleration_Y, acceleration_Z, ppgData);
+    sendDataToInfluxDB(sensorValue, acceleration_X, acceleration_Y, acceleration_Z, sp02Avg, beatAvg);
   }
 }
